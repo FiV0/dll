6ec8ba6bced43a4e0a1aaf797a74722e2310519b
FiV0 <finn.volkel@gmail.com>
2021-04-16 18:00:18 +0100

    Initial commit

---
+++ src/main/java/ramper/util/DoublyLinkedList.java
@@ -0,0 +1,114 @@
+package ramper.util;
+
+/**
+ * A doubly linked list implementation that exposes delete and add in O(1).
+ * Note that this implementation is not synchronized.
+ */
+public class DoublyLinkedList<E>
+{
+    public class Node {
+        E item;
+        Node next, prev = null;
+
+        public Node() {
+            this.item = null;
+        }
+
+        public Node(E item) {
+            this.item = item;
+        }
+
+        E getItem() {
+            return this.item;
+        }
+
+        protected void addBefore(Node next) {
+            if (next.prev != null) {
+                next.prev.next = this;
+            }
+            this.prev = next.prev;
+            next.prev = this;
+            this.next = next;
+        }
+
+        protected void unlink() {
+            if (this.prev != null) {
+                this.prev.next = this.next;
+            }
+            if (this.next != null) {
+                this.next.prev = this.prev;
+            }
+        }
+    };
+
+    Node head, tail = null;
+    int size = 0;
+
+    Node getHead() {
+        return this.head;
+    }
+
+    Node getTail() {
+        return this.tail;
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    void add(Node node) {
+        node.next = this.head;
+        if (this.head != null) this.head.prev = node;
+        if (this.tail == null) this.tail = node;
+        this.head = node;
+        this.size++;
+    }
+
+    Node pop() {
+        Node res = this.tail;
+        if (this.tail != null) {
+            this.tail = this.tail.prev;
+            // only one item in the list
+            if (this.tail == null) {
+                this.head = null;
+            } else {
+                this.tail.next = null;
+            }
+            this.size--;
+        }
+        return res;
+    }
+
+    void pop(int n) {
+        while (n-- != 0 && this.tail != null) {
+            this.tail = this.tail.prev;
+            this.size--;
+        }
+        if (this.tail == null) {
+            this.head = null;
+        } else {
+            this.tail.next = null;
+        }
+    }
+
+    void remove (Node node) {
+        if (this.head == node) {
+            this.head = node.next;
+        }
+        if (this.tail == node) {
+            this.tail = node.prev;
+        }
+        node.unlink();
+        this.size--;
+    }
+
+    void touch (Node node) {
+        this.remove(node);
+        this.add(node);
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Doubly Linked List" );
+    }
+}
---
+++ src/test/java/ramper/util/DoublyLinkedListTest.java
@@ -0,0 +1,111 @@
+package ramper.util;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+import ramper.util.DoublyLinkedList.Node;
+
+import java.util.ArrayList;
+
+/**
+ * Unit test for DoublyLinkedList.
+ */
+public class DoublyLinkedListTest
+{
+    public <E> ArrayList<E> doublyLinkedListAsArray(DoublyLinkedList<E> dll) {
+        ArrayList<E> res = new ArrayList<E>(dll.getSize());
+        while (dll.getSize() > 0) {
+            res.add(dll.pop().getItem());
+        }
+        return res;
+    }
+
+    @Test
+    public void simpleAdds()
+    {
+        DoublyLinkedList<Integer> dll = new DoublyLinkedList<Integer>();
+        dll.add(dll.new Node(1));
+        dll.add(dll.new Node(2));
+        dll.add(dll.new Node(3));
+        ArrayList<Integer> res = doublyLinkedListAsArray(dll);
+        assertArrayEquals(new Integer [] {1,2,3}, res.toArray());
+    }
+
+    @Test
+    public void addsAndPops()
+    {
+        DoublyLinkedList<Integer> dll = new DoublyLinkedList<Integer>();
+        dll.add(dll.new Node(1));
+        dll.add(dll.new Node(2));
+        dll.add(dll.new Node(3));
+        dll.pop();
+        dll.pop();
+        dll.pop();
+        assertEquals(0, dll.getSize());
+    }
+
+
+    @Test
+    public void addAndPopInterleave()
+    {
+        DoublyLinkedList<Integer> dll = new DoublyLinkedList<Integer>();
+        dll.add(dll.new Node(1));
+        dll.pop();
+        dll.add(dll.new Node(2));
+        dll.add(dll.new Node(3));
+        dll.pop();
+        dll.pop();
+        assertEquals(0, dll.getSize());
+    }
+
+    @Test
+    public void remove()
+    {
+        DoublyLinkedList<Integer> dll = new DoublyLinkedList<Integer>();
+        dll.add(dll.new Node(1));
+        DoublyLinkedList<Integer>.Node n2 = dll.new Node(2);
+        dll.add(n2);
+        dll.add(dll.new Node(3));
+        dll.remove(n2);
+        DoublyLinkedList<Integer>.Node n4 = dll.new Node(4);
+        dll.add(n4);
+        dll.add(dll.new Node(5));
+        dll.remove(n4);
+        ArrayList<Integer> res = doublyLinkedListAsArray(dll);
+        assertArrayEquals(new Integer [] {1,3,5}, res.toArray());
+    }
+
+    // corner cases
+
+    @Test
+    public void emptyPop()
+    {
+        DoublyLinkedList<Integer> dll = new DoublyLinkedList<Integer>();
+        dll.add(dll.new Node(1));
+        dll.pop();
+        assertEquals(null, dll.pop());
+        assertEquals(0, dll.getSize());
+        ArrayList<Integer> res = doublyLinkedListAsArray(dll);
+        assertArrayEquals(new Integer [] {}, res.toArray());
+    }
+
+
+    @Test
+    public void frontRemove()
+    {
+        DoublyLinkedList<Integer> dll = new DoublyLinkedList<Integer>();
+        DoublyLinkedList<Integer>.Node n1 = dll.new Node(1);
+        dll.add(n1);
+        dll.add(dll.new Node(2));
+        assertEquals(2, dll.getSize());
+        dll.remove(n1);
+        ArrayList<Integer> res = doublyLinkedListAsArray(dll);
+        assertEquals(1, dll.getSize());
+        assertArrayEquals(new Integer [] {2}, res.toArray());
+    }
+
+
+}
